7a227f8b8eea7dd7f71ab7bc5f57e405a45057f2
ugsrv <ugsrv@student.kit.edu>
2025-09-15 21:23:23 +0200

    Add main application entry point with command line argument validation

---
+++ src/edu/kit/kastle/filesorter/Application.java
@@ -0,0 +1,30 @@
+package edu.kit.kastle.filesorter;
+
+/**
+ * The class offering the entry point for the application.
+ *
+ * @author ugsrv
+ * @author Programmieren-Team
+ */
+public class Application {
+
+    private static final String ERROR_MESSAGE_COMMAND_LINE_ARGUMENTS = "Error: Expected no command line arguments";
+    private static final String WELCOME_MESSAGE = "Use one of the following commands : load , input , tokenization , analyze , clear , list , top , matches , histogram , edit , inspect , quit .";
+
+    private Application() {
+        // utility class
+    }
+
+    /**
+     * The entry point for the application. No command line arguments are expected.
+     *
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.err.println(ERROR_MESSAGE_COMMAND_LINE_ARGUMENTS);
+            return;
+        }
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
